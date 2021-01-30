7596737c187b1089d8fc9ee1b7fc41095fd1e607
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-01-30 15:25:12 -0300

    Added a prompt input that asks what is the new grid size the user wants and stores this input.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const container = document.querySelector(".container");
+const button = document.querySelector(".button");
 
 let squares = [];
+let userInput = 0;
 
 //Generate the 16x16 grid
 for (let i = 0; i < 256; i++) {
@@ -15,3 +17,11 @@ for (let i = 0; i < 256; i++) {
         squares[i].style.backgroundColor = "black";
     });
 }
+
+//Clears the grid when the button is clicked
+button.addEventListener("click", function(){
+    for (let i = 0; i < 256; i++) {
+        squares[i].style.backgroundColor = "aquamarine";;
+    }
+    userInput = prompt("What is the size of the new grid?", 16);
+})
