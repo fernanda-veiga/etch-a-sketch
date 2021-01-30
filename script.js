62ed40b951ddf55ffaf3bae7a8cd20407c116391
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-01-30 15:30:10 -0300

    Created a function called generateGrid that receives a number and generates a grid with that number.

--- script.js
+++ script.js
@@ -4,12 +4,8 @@ const button = document.querySelector(".button");
 let squares = [];
 let userInput = 0;
 
-//Generate the 16x16 grid
-for (let i = 0; i < 256; i++) {
-    squares[i] = document.createElement("div");
-    squares[i].classList.add("square");
-    container.appendChild(squares[i]);
-}
+//Generate the initial 16x16 grid
+generateGrid(16);
 
 //Change the background color when the mouse hovers over the square
 for (let i = 0; i < 256; i++) {
@@ -25,3 +21,12 @@ button.addEventListener("click", function(){
     }
     userInput = prompt("What is the size of the new grid?", 16);
 })
+
+//FUNCTIONS
+function generateGrid(userInput) {
+    for (let i = 0; i < Math.pow(userInput, 2); i++) {
+        squares[i] = document.createElement("div");
+        squares[i].classList.add("square");
+        container.appendChild(squares[i]);
+    }
+}
