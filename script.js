411d32355e7cc60230f7a0d174c05a8fc3c3686e
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-01-30 15:03:27 -0300

    Added a for loop to change the background color of the square to black when the mouse hovers over it.

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ const container = document.querySelector(".container");
 
 let squares = [];
 
-for (i = 0; i < 256; i++) {
+//Generate the 16x16 grid
+for (let i = 0; i < 256; i++) {
     squares[i] = document.createElement("div");
     squares[i].classList.add("square");
     container.appendChild(squares[i]);
+}
+
+//Change the background color when the mouse hovers over the square
+for (let i = 0; i < 256; i++) {
+    squares[i].addEventListener("mouseenter", function(){
+        squares[i].style.backgroundColor = "black";
+    });
 }
